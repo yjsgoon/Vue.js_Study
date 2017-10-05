25b8eb8fe4878d0eb56083e30e759eb87ebcda2f
JiSoo Yoon <yjsgoon@gmail.com>
2017-10-05 17:23:22 +0900

    Complete chapter2-1 question

--- chapter2/3_plugin.js
+++ chapter2/3_plugin.js
@@ -7,5 +7,14 @@ export default {
         Vue.directive('sqrt', function (el, binding) {
             el.innerHTML = Math.sqrt(binding.value);
         });
+        Vue.directive('sin', function (el, binding) {
+            el.innerText = Math.sin(binding.value);
+        });
+        Vue.directive('cos', function (el, binding) {
+            el.innerText = Math.cos(binding.value);
+        });
+        Vue.directive('tan', function (el, binding) {
+            el.innerText = Math.tan(binding.value);
+        });
     }
 };
--- chapter2/main.js
+++ chapter2/main.js
@@ -13,6 +13,15 @@ exports.default = {
         Vue.directive('sqrt', function (el, binding) {
             el.innerHTML = Math.sqrt(binding.value);
         });
+        Vue.directive('sin', function (el, binding) {
+            el.innerText = Math.sin(binding.value);
+        });
+        Vue.directive('cos', function (el, binding) {
+            el.innerText = Math.cos(binding.value);
+        });
+        Vue.directive('tan', function (el, binding) {
+            el.innerText = Math.tan(binding.value);
+        });
     }
 };
 
